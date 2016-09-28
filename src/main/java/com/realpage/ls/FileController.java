f0355a00d8be67e31583f4b41988426de6a5f51f
Iqbal Yusuff Personal <iqbalyusuff@hotmail.com>
2016-09-28 15:45:20 -0500

    More stacktrace on FileController.

--- src/main/java/com/realpage/ls/FileController.java
+++ src/main/java/com/realpage/ls/FileController.java
@@ -41,6 +41,7 @@ public class FileController {
         }
         catch(FileNotFoundException ex) {
             logger.debug("FileController: Unable to open file '" + filePath + "'");                
+            logger.debug(ex.getStackTrace());
         }
         catch(IOException ex) {
         	logger.debug("FileController: Error reading file '" + filePath + "'");                  
